575f3008acdb1bd0be2ef4dff36dab89481db4b3
vetrugan <vetrugan12@gmail.com>
2017-08-31 21:24:29 +0300

    Added view to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
