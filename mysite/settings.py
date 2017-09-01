bc4c50d1bd52575f1bb2d90f35baff62002c85b8
vetrugan <vetrugan12@gmail.com>
2017-09-01 20:08:52 +0300

    DEBUG = False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!5d0uf!5b2%)=9u$vf^93@e1j)0bt*%n4i1!%h_a)75apdo(f_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'vatrugan.pythonanywhere.com']
 
