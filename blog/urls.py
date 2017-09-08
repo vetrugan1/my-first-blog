de4fc5114a222e23fb676b13f2d9d7fffc22823f
vetrugan <vetrugan12@gmail.com>
2017-09-08 15:26:17 +0300

    Add the favicon.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -12,4 +15,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
     url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
     url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+    url(r'^favicon\.ico$', favicon_view),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/favicon.png/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
